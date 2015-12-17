294f6db51b3d7643968aaa658e2fc171df336345
ivanzy <idimitryz@gmail.com>
2015-12-17 14:09:20 -0200

    mudanças na classe File

--- ImpressContext/src/main/java/br/ufabc/impress/file/File.java
+++ ImpressContext/src/main/java/br/ufabc/impress/file/File.java
@@ -5,16 +5,24 @@ import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.PrintWriter;
 import java.io.UnsupportedEncodingException;
+import java.sql.Timestamp;
 
 public class File {
 
-	public void write(String fileName, String line) {
+	public void write(String fileName, String m) {
 
 		try {
-			// O parametro é que indica se deve sobrescrever ou continua no
-			// arquivo.
+			// O parametro é que indica se deve sobrescrever ou continua no arquivo.
+	        String time = new Timestamp(System.currentTimeMillis()).toString();
+	        String[] msg = m.split(";");
 			FileWriter fw = new FileWriter(fileName, true);
 			BufferedWriter conexao = new BufferedWriter(fw);
+			String line = null;
+			for(int i = 0; i < msg.length; i++){
+				line = line + "\"" + msg[i] + "\"";
+				
+			}
+			line = line +  "\"" + time + "\"";
 			conexao.write(line);
 			conexao.newLine();
 			conexao.close();
