12a84d970c92d03f66ccd7742789e9043ef9c5f4
Isaac Pinheiro <isaacpnhr@gmail.com>
2016-02-18 09:55:16 -0200

    Adicionando type no monitor.

--- restapi/src/main/webapp/script/contextmonitor.js
+++ restapi/src/main/webapp/script/contextmonitor.js
@@ -4,15 +4,24 @@ app.controller('contextMonitorController', function($scope){
 	
 	$scope.listStatus = true;
 	$scope.graphStatus = false;
+	$scope.typeStatus = false;
 	
 	$scope.changeToList = function(){
 		$scope.listStatus = true;
 		$scope.graphStatus = false;
+		$scope.typeStatus = false;
 	};
 	
 	$scope.changeToGraph = function(){
 		$scope.listStatus = false;
 		$scope.graphStatus = true;
+		$scope.typeStatus = false;
+	};
+	
+	$scope.changeToType = function(){
+		$scope.listStatus = false;
+		$scope.graphStatus = false;
+		$scope.typeStatus = true;
 	};
 	
 });
